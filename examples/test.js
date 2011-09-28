7f4a3d45222cdb229559f4ddf4a9942d86d23b30
Nick Campbell <nicholas.j.campbell@gmail.com>
2011-09-28 01:03:43 -0400

    Fixed up example to work again... Also fixed and updated lib to use startNext instead of just next.
    Signed-off-by: Nick Campbell <nicholas.j.campbell@gmail.com>

--- examples/test.js
+++ examples/test.js
@@ -1,21 +1,27 @@
 var Queue = require('../lib/queuestream').QueueStream(),
     fs = require('fs');
 
-stream.on('error', function(err) {
+Queue.on('error', function(err) {
   console.log('err: ' + err);
 });
 
-stream.on('data', function(data) {
-  console.log(data.toString());
+Queue.on('data', function(data) {
+  console.log('listener 1: ' + data.toString());
+});
+Queue.on('data', function(data) {
+  console.log('listener 2: ' + data.toString());
+});
+Queue.on('data', function(data) {
+  console.log('listener 3: ' + data.toString());
 });
 
-stream.on('end', function() {
+Queue.on('end', function() {
   console.log('done');
 });
 
 var test1 = fs.createReadStream(__dirname + '/files/test1.txt');
-stream.queue(test1);
+Queue.queue(test1);
 var test2 = fs.createReadStream(__dirname + '/files/test2.txt');
-stream.queue(test2);
+Queue.queue(test2);
 
-stream.start();
+Queue.startNext();
